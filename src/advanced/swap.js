6214a90bd08aa336ea06ee4486060c5d93df9a05
Tuva Aarseth <tuva.emilie@hotmail.com>
2024-09-20 10:05:20 +0200

    completed assignment and extension

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -2,5 +2,8 @@ let a = 8
 let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
+const temp = a
+a = b
+b = temp
 
 module.exports = { a, b }
--- src/declaration.js
+++ src/declaration.js
@@ -3,6 +3,9 @@
 //
 // TODO: 1. Declare the variables firstName and age so that the tests pass
 
+const firstName = 'Jane'
+const age = 35
+
 // do not edit below this line
 let firstNameExport = ''
 try {
